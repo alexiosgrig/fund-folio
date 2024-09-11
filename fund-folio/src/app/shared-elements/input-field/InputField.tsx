896f5f82047008a4e091e98800728db0e908e485
agrigo <agrigo@netcompany.com>
2024-09-12 01:00:14 +0300

    Enhance InputField

--- fund-folio/src/app/shared-elements/input-field/InputField.tsx
+++ fund-folio/src/app/shared-elements/input-field/InputField.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const InputField = () => {
+export const InputField = ({ onSubmit }: any) => {
+  const [field, setField] = useState('');
   return (
     <>
       <label htmlFor="username" className="block text-lg font-medium mb-2">
         Username:
       </label>
       <input
+        onChange={(e) => setField(e.target.value)}
         type="text"
         id="username"
         name="username"
         required
         className="block w-80 px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
       />
+      <button type="submit" onClick={() => onSubmit(field)}>
+        {' '}
+        Submit{' '}
+      </button>
     </>
   );
 };
