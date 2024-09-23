0b28cfc8e8a1dcb42c2df062b390589196652ac7
agrigo <agrigo@netcompany.com>
2024-09-24 01:31:11 +0300

    Add new interface

---
+++ fund-folio/src/app/services/INewsData.ts
@@ -0,0 +1,15 @@
+export interface INewsPayload {
+  category: string;
+}
+
+export interface INewsResponse {
+  category: string;
+  datetime: number;
+  headline: string;
+  id: number;
+  image: string;
+  related: string;
+  source: string;
+  summary: string;
+  url: string;
+}
