a13e3e6418581e087fec48ded5b05ea0ac5b2c68
agrigo <agrigo@netcompany.com>
2024-09-24 01:27:12 +0300

    Add i18n on main

--- fund-folio/src/main.tsx
+++ fund-folio/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/router';
+import './i18n';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
