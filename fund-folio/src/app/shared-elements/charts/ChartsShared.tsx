d4a2f1f081981895c9d9d08d857852dd8391eeaf
agrigo <agrigo@netcompany.com>
2024-09-24 01:32:09 +0300

    Refactor Charts component.

--- fund-folio/src/app/shared-elements/charts/ChartsShared.tsx
+++ fund-folio/src/app/shared-elements/charts/ChartsShared.tsx
@@ -1,79 +1,31 @@
 import { BarChart } from '@mui/x-charts';
-import React, { useCallback, useEffect, useState } from 'react';
-import { getRecommendationsData } from '../../services/services';
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 
-interface IRecommendationMapData {
+export interface IRecommendationMapData {
   data: number[];
   stack: string;
   label: string;
 }
 
 interface ChartsProps {
-  symbol: string;
+  series: IRecommendationMapData[] | undefined;
+  loading: boolean;
 }
 
-export const ChartsShared: React.FC<ChartsProps> = ({ symbol }) => {
-  const [series, setSeries] = useState<IRecommendationMapData[] | undefined>(
-    undefined
-  );
-
-  const getRecommendations = useCallback(async () => {
-    try {
-      const response = await getRecommendationsData({ symbol });
-      if (response && response.length > 0) {
-        const strongBuyData = [];
-        const buyData = [];
-        const holdData = [];
-        const sellData = [];
-        const strongSell = [];
-
-        for (const data of response) {
-          strongBuyData.push(data.strongBuy);
-          buyData.push(data.buy);
-          holdData.push(data.hold);
-          sellData.push(data.sell);
-          strongSell.push(data.strongSell);
-        }
-
-        const newRecommendationsArray: IRecommendationMapData[] = [
-          {
-            data: strongBuyData,
-            stack: '1',
-            label: 'Strong Buy',
-          },
-          { data: buyData, stack: '2', label: 'Buy' },
-          { data: holdData, stack: '3', label: 'Hold' },
-          { data: sellData, stack: '4', label: 'Sell' },
-          {
-            data: strongSell,
-            stack: '5',
-            label: 'Strong Sell',
-          },
-        ];
-        setSeries(newRecommendationsArray);
-      } else {
-        console.warn('No data available');
-      }
-    } catch (err) {
-      console.warn('Error fetching recommendations:', err);
-    }
-  }, [symbol]);
-  useEffect(() => {
-    getRecommendations();
-  }, [getRecommendations]);
-
+export const ChartsShared: React.FC<ChartsProps> = ({ series, loading }) => {
   return (
     <div>
-      {series ? (
+      {loading ? (
+        <CircularProgress color="error" />
+      ) : (
         <BarChart
           xAxis={[{ scaleType: 'band', data: ['Recommendations'] }]}
-          series={series}
+          series={series ? series : []}
           width={600}
           height={500}
           barLabel="value"
         />
-      ) : (
-        <p>Loading chart data...</p>
       )}
     </div>
   );
