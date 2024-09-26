88d0f7b496f0786c84e98a28cdda67b0914f7a8f
agrigo <agrigo@netcompany.com>
2024-09-27 02:23:00 +0300

    Introduce new table component

---
+++ fund-folio/src/app/shared-elements/table/TableSheetShared.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { FinancialDataAsReportedResponse } from 'src/app/services/IFinancialData';
+
+interface TableSheetSharedProps {
+  data: FinancialDataAsReportedResponse[];
+  statement: 'bs' | 'cf' | 'ic';
+}
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+export default function TableSheetShare({
+  data,
+  statement,
+}: TableSheetSharedProps) {
+  const bs = data?.map((row) => row.report.bs);
+  const cf = data?.map((row) => row.report.cf);
+  const ic = data?.map((row) => row.report.ic);
+
+  const dataToShow = () => {
+    if (statement === 'bs') {
+      return bs;
+    } else if (statement === 'cf') {
+      return cf;
+    } else if (statement === 'ic') {
+      return ic;
+    } else {
+      return [];
+    }
+  };
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>{'Description'}</StyledTableCell>
+            {data?.slice(0, 4).map((row) => (
+              <StyledTableCell>{`Acceptance Date: ${
+                row.acceptedDate.split(' ')[0]
+              }`}</StyledTableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {dataToShow()?.map((row, index) => (
+            <StyledTableRow key={index}>
+              <StyledTableCell>{row[index]?.label}</StyledTableCell>
+              {row.slice(0, 4).map((element) => (
+                <StyledTableCell>
+                  {element.value.toLocaleString()}
+                </StyledTableCell>
+              ))}
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
