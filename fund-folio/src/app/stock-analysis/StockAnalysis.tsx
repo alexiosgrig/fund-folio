058aefebdbde1b6a7e79c958874fdcf96921a50d
agrigo <agrigo@netcompany.com>
2024-09-27 02:21:28 +0300

    Refactor stock analysis

--- fund-folio/src/app/stock-analysis/StockAnalysis.tsx
+++ fund-folio/src/app/stock-analysis/StockAnalysis.tsx
@@ -1,48 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { InputField } from '../shared-elements/input-field/InputField';
 import { useTranslation } from 'react-i18next';
-import { Accordion, AccordionSummary, Grid2, Typography } from '@mui/material';
+import {
+  Alert,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CircularProgress,
+  Grid2,
+  Snackbar,
+} from '@mui/material';
+import { AccordionShared } from '../shared-elements/accordion/AccordionShared';
+import { getFinancialAsReportedData } from '../services/services';
+import { FinancialDataAsReportedPayload } from '../services/IFinancialData';
+import { SelectField } from '../shared-elements/select-field/SelectField';
+import { StockAnalysisPaper } from './StockAnalysisPaper';
 
 export const StockAnalysis = () => {
   const { t } = useTranslation();
+  const [data, setData] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({});
   const methods = useForm();
+
+  const statementList = [
+    { label: t('freeCashFlow'), statement: 'fc' },
+    { label: t('incomeStatement'), statement: 'is' },
+    { label: t('balanceSheet'), statement: 'is' },
+  ];
+
+  const list = [
+    { value: 'annual', label: 'Annual' },
+    { value: 'quarterly', label: 'Quarterly' },
+  ];
+
+  const searchStock = async (payload: FinancialDataAsReportedPayload) => {
+    setLoading(true);
+    try {
+      const res = await getFinancialAsReportedData(payload);
+      setData(res);
+      setLoading(false);
+    } catch (e) {
+      setError(e);
+      console.error(e);
+    }
+  };
+
   return (
     <FormProvider {...methods}>
-      <Grid2
-        spacing={2}
-        container
-        alignContent={'center'}
-        alignItems={'center'}
-        alignSelf={'center'}
-      >
-        <Grid2 size={6}>
-          <InputField
-            id={'id'}
-            label={t('enterStock')}
-            name={'id'}
-            variant={'filled'}
-            color="error"
-          />
-        </Grid2>
-        <Grid2 size={6}>
-          <InputField
-            id={'id'}
-            label={t('enterStock')}
-            name={'id'}
-            variant={'filled'}
-            color="error"
-          />
-        </Grid2>
-        <Grid2 size={12}>
-          <Accordion>
-            <AccordionSummary aria-controls="panel1-content" id="panel1-header">
-              <Typography>Accordion 2</Typography>
-            </AccordionSummary>
-            <Typography>Accordion 2</Typography>
-          </Accordion>
-        </Grid2>
-      </Grid2>
+      <Card sx={{ padding: '100px' }}>
+        <CardContent>
+          <Grid2
+            spacing={8}
+            container
+            alignContent={'center'}
+            alignItems={'center'}
+            alignSelf={'center'}
+          >
+            <Grid2 size={12}>
+              <Card>
+                <CardContent>
+                  <Grid2 container spacing={12}>
+                    <Grid2 size={6}>
+                      <InputField
+                        id={'symbol'}
+                        label={t('enterStock')}
+                        name={'symbol'}
+                        variant={'filled'}
+                        color="error"
+                      />
+                    </Grid2>
+                    <Grid2 size={6}>
+                      <SelectField list={list} label="Annual" name="freq" />
+                    </Grid2>
+                  </Grid2>
+                  <CardActions
+                    sx={{ justifyContent: 'flex-end', padding: '50px' }}
+                  >
+                    <Button
+                      variant={'contained'}
+                      color={'secondary'}
+                      onClick={methods.handleSubmit(searchStock)}
+                    >
+                      {t('search')}
+                    </Button>
+                  </CardActions>
+                </CardContent>
+              </Card>
+            </Grid2>
+            {loading ? (
+              <CircularProgress color="error" />
+            ) : (
+              <Card>
+                <CardContent>
+                  <Grid2 container spacing={8}>
+                    {statementList.map((e) => (
+                      <Grid2 size={12}>
+                        <StockAnalysisPaper
+                          statement={e.statement}
+                          label={e.label}
+                          data={data}
+                        />
+                      </Grid2>
+                    ))}
+                  </Grid2>
+                </CardContent>
+              </Card>
+            )}
+          </Grid2>
+        </CardContent>
+      </Card>
+      <Snackbar open={!!error?.message} autoHideDuration={1}>
+        <Alert severity="error" variant="filled" sx={{ width: '100%' }}>
+          {error?.message}
+        </Alert>
+      </Snackbar>
     </FormProvider>
   );
 };
