44b13ca399530365acd3923d5b0d34c949b9979b
agrigo <agrigo@netcompany.com>
2024-09-28 00:58:19 +0300

    Add provider to application

--- fund-folio/src/main.tsx
+++ fund-folio/src/main.tsx
@@ -3,13 +3,16 @@ import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/router';
 import './i18n';
-
+import { Provider } from 'react-redux';
+import { store } from './app/store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
